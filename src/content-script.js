c03d4dd88652f2a173c66f72c6cd470b91d8d64c
David J. Burger <davidburger@gmail.com>
2024-01-26 08:09:57 -0700

    Add links for default plays filtering
    Hook the page so that links are added for a series of
    default filterings at different minimum EVs.

--- src/content-script.js
+++ src/content-script.js
@@ -1,5 +1,19 @@
-/*
-window.addEventListener('click', function(evt) {
-  window.alert("hello world");
-});
-*/
+const playsDiv = (minEv) => {
+  const a = document.createElement("a");
+  a.setAttribute(
+    "href",
+    `/search/plays?search=&group=Y&bet=Y&ways=1&ev=${minEv}&arb=0&sort=1&max=250&width=6.5%25`);
+  a.appendChild(document.createTextNode(`${minEv}`));
+
+  const div = document.createElement("div");
+  div.setAttribute("class", "nav");
+  div.appendChild(a);
+
+  return div;
+};
+
+const settingsDiv = document.querySelector('a[href="/settings"]').parentElement;
+const topNav = settingsDiv.parentElement;
+topNav.insertBefore(playsDiv(3), settingsDiv.nextSibling);
+topNav.insertBefore(playsDiv(4), settingsDiv.nextSibling);
+topNav.insertBefore(playsDiv(5), settingsDiv.nextSibling);
