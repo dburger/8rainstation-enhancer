a844eeb9e20f965f3c5d12fd65bfa86b769074af
David J. Burger <davidburger@gmail.com>
2024-02-02 08:59:34 -0700

    Load options page books table from dummy data

--- src/options.js
+++ src/options.js
@@ -1,10 +1,19 @@
 const loadSettings = (settings) => {
     console.log("loading settings", settings);
+    const booksTable = document.getElementById("books");
+    const tbody = booksTable.querySelector("tbody");
+
     // Array.from(settings) and then sort by the first element, I think.
     // actually it is in settings.settings, do you want that?
-    for (const [key, value] of Object.entries(settings)) {
-        console.log("key", key);
-        console.log("value", value);
+    for (const [key, value] of Object.entries(settings.settings)) {
+        const tr = document.createElement("tr");
+        let td = document.createElement("td");
+        td.innerText = key;
+        tr.appendChild(td);
+        td = document.createElement("td");
+        td.innerText = value.urlTemplate;
+        tr.appendChild(td);
+        tbody.appendChild(tr);
     }
 };
 
