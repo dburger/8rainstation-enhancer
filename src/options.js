875552330f8337097a7fe20a42c4a68d7aa45c10
David J. Burger <davidburger@gmail.com>
2024-02-02 10:08:35 -0700

    Have options page display books data in text inputs

--- src/options.js
+++ src/options.js
@@ -1,19 +1,31 @@
+const createInputText = (value) => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.value = value;
+    return input;
+};
+
+const createBooksRowTd = (value) => {
+    const td = document.createElement("td");
+    td.appendChild(createInputText(value));
+    return td;
+}
+
+const createBooksRow = (key, urlTemplate) => {
+    const tr = document.createElement("tr");
+    tr.appendChild(createBooksRowTd(key));
+    tr.appendChild(createBooksRowTd(urlTemplate));
+    return tr;
+};
+
 const loadSettings = (settings) => {
-    console.log("loading settings", settings);
     const booksTable = document.getElementById("books");
     const tbody = booksTable.querySelector("tbody");
 
     // Array.from(settings) and then sort by the first element, I think.
     // actually it is in settings.settings, do you want that?
     for (const [key, value] of Object.entries(settings.settings)) {
-        const tr = document.createElement("tr");
-        let td = document.createElement("td");
-        td.innerText = key;
-        tr.appendChild(td);
-        td = document.createElement("td");
-        td.innerText = value.urlTemplate;
-        tr.appendChild(td);
-        tbody.appendChild(tr);
+      tbody.appendChild(createBooksRow(key, value.urlTemplate));
     }
 };
 
